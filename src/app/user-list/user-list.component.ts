e486b705d0ae4c7e5cffca2895e73dc8810b9ae1
Harry <harryfountedakis@gmail.com>
2025-09-28 13:01:05 +0300

    Unsubscribed

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from '../user.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { User } from '../user.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
+  standalone: true,
   imports: [CommonModule, RouterModule],
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
   users: User[] = [];
+  private hihi!: Subscription;
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe((lala: User[]) => {
+    this.hihi = this.userService.getUsers().subscribe((lala: User[]) => {
       console.log(lala);
-      
       this.users = lala;
     });
   }
- 
-  //TODO unsubscribe 
+
+  ngOnDestroy(): void {
+    if (this.hihi) {
+      this.hihi.unsubscribe();
+    }
+  }
 }
